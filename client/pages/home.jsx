6d51bd976e677ff9ae782d8012b0cdb8c6473419
Peter Vila <peterdvila@gmail.com>
2021-12-07 02:46:04 +0000

    Fetched data from example api and saved into state during mount. Mapped through state array for data in separated format

--- client/pages/home.jsx
+++ client/pages/home.jsx
@@ -1,14 +1,49 @@
-import React, { useState } from 'react';
+import React, {
+  useState
+} from 'react';
 
-export default function Home(props) {
-  const [counter, addCounter] = useState(0);
-  const increment = () => {
-    addCounter(counter + 1);
-  };
-  return (
-    <>
-      <h1>{counter}</h1>
-      <button onClick={increment}>Add</button>
-    </>
-  );
+export default class Home extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      users: null
+    };
+  }
+
+  componentDidMount() {
+    fetch('https://api.hatchways.io/assessment/students')
+      .then(res => res.json())
+      .then(result => {
+        this.setState({
+          users: result
+        });
+      });
+  }
+//Display Logo, Name(h1), email, company, skill, Average
+
+  render() {
+    const users = this.state.users && this.state.users.students.map((user, index) => {
+      const scores = user.grades.reduce((total, current) => {
+        return (parseInt(total, 10) + parseInt(current, 10)/10);
+      }, 0);
+      const match = (
+        <div>
+          <img src={user.pic}/>
+          <h3>{`${user.firstName}  ${user.lastName}`}</h3>
+          <p>{`Email: ${user.email}`}</p>
+          <p>{`Company: ${user.company}`}</p>
+          <p>{`Skill: ${user.skill}`}</p>
+          <p>{`Average: ${scores}%`}</p>
+        </div>
+      );
+      return (
+        <div key={index}>{match}</div>
+      );
+    });
+    return (
+      <>
+      <div>{users}</div>
+      </>
+    );
+  }
 }
